75cdb12ba46bc7a7f8186d26d162409eabfd5b50
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-16 14:52:13 +0100

    [MainContent] Added flex wrapper element for game info panel

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import GameBoard from './GameBoard'
 
+// Render all content in the <main> tag, including the game board
 function MainContent(props) {
     return (
         <main className="justify-center">
             <div className="width-limit">
-                <button type="button" onClick={() => props.onNewGame()}>New Game</button>
-                <h1>{props.gameState.message}</h1>
+                <div class="info-area">
+                    <button type="button" onClick={() => props.onNewGame()}>New Game</button>
+                    <h1>{props.gameState.message}</h1>
+                </div>
                 <GameBoard gameState={props.gameState} onCellClick={props.onCellClick} />
             </div>
         </main>
