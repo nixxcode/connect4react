39e2d1c7279b5dc0db35515522cba2b23dffdcc9
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-23 13:52:02 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ function Footer() {
     return (
         <footer className="justify-center">
             <div className="width-limit">
-                <span>Connect 4 React 2019 © Dominik Petrovic (<a href="https://github.com/nixxcode/connect4react">view code on Github</a>), CSS/visuals borrowed from <a href="https://codepen.io/jeffleu/pen/KgbZwj">Jeff Leu</a> under the MIT license</span>
+                <span>Connect 4 React 2019 © Dominik Petrovic (<a href="https://github.com/nixxcode/connect4react">view code on Github</a>)</span> 
+                <br/>
+                <span>CSS/visuals borrowed from <a href="https://codepen.io/jeffleu/pen/KgbZwj">Jeff Leu</a> under the MIT license</span>
             </div>
         </footer>
     )
