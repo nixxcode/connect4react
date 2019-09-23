c8cad9967753aa7548b4b2043cff8a98fb803fee
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-23 12:57:18 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="justify-center">
             <div className="width-limit">
-                <span>Connect 4 React 2019 © Dominik Petrovic</span>
+                <span>Connect 4 React 2019 © Dominik Petrovic, CSS/visuals borrowed from <a href="https://codepen.io/jeffleu/pen/KgbZwj">Jeff Leu</a> under the MIT license</span>
             </div>
         </footer>
     )
